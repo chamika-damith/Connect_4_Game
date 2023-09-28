86fde9372449e5e370f574dad1e9141b235cafcb
chamika-damith <chamikadamith9@gmail.com>
2023-09-28 12:41:45 +0530

    first commit

--- connect-four-game-assignment-main/src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ connect-four-game-assignment-main/src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -1,5 +1,18 @@
 package lk.ijse.dep.service;
 
 public class BoardImpl {
+    private Piece piece[][];
+    BoardUI boardUI;
 
+    public BoardImpl(BoardUI boardUI) {
+        this.boardUI = boardUI;
+    }
+
+    public BoardUI getBoardUI() {
+        return boardUI;
+    }
+
+    public void setBoardUI(BoardUI boardUI) {
+        this.boardUI = boardUI;
+    }
 }
